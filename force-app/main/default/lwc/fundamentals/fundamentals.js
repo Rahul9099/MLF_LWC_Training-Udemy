3787f9976dbc4aa3d787a2fa42925566aaebbedb
Rahul9099 <rahuldange208@gmail.com>
2024-04-22 13:22:10 +0530

    going to start lyf cycle hooks

--- force-app/main/default/lwc/fundamentals/fundamentals.js
+++ force-app/main/default/lwc/fundamentals/fundamentals.js
@@ -25,6 +25,19 @@ export default class Fundamentals extends LightningElement {
             name:'bez'
         }
     ]
+    isLoaded = false
+    renderedCallback(){
+        if(this.isLoaded){return}
+
+        const style =document.createElement('style');
+        style.innerText = `c-shadow-dom-styling .slds-button{
+            background: red;
+            color: white;
+        }`
+        this.template.querySelector('lightning-button').appendChild(style);
+        this.isLoaded = true;
+    }
+
     handleCityChange(event){
     //  this.person = {...this.person,'city':event.target.value};
      this.person.city =event.target.value;
