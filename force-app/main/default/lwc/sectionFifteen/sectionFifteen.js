b0b0ad5280d91e607d17210bfa526d2b02073ab7
Rahul9099 <rahuldange208@gmail.com>
2024-04-23 12:40:01 +0530

    section 15 partial complete need to revisit and skipp the last 4 videos (incomplete)

---
+++ force-app/main/default/lwc/sectionFifteen/sectionFifteen.js
@@ -0,0 +1,35 @@
+import { LightningElement } from 'lwc';
+import img from '@salesforce/resourceUrl/DemoImage'
+
+// content assest
+import tcFile from '@salesforce/contentAssetUrl/tcFile'
+
+// Internationalization
+import internationalizationPropertyName from '@salesforce/i18n/internationalizationProperty'
+import currency from '@salesforce/i18n/currency'
+
+// local
+import local from '@salesforce/i18n/locale'
+
+// permission check
+import hasPermission from '@salesforce/userPermission/ViewAllData';
+export default class SectionFifteen extends LightningElement {
+
+    get image(){
+        return img;
+    }
+    get file(){
+        return tcFile;
+    }
+    get hasPermission(){
+        return hasPermission
+    }
+/*
+    num = 8347.58;
+    formatedNum = new Intl.NumberFormat(local,{
+        style:'currency',
+        currency:currency,
+        compactDisplay:'symbol'
+    }).format(this.num);
+*/    
+}
